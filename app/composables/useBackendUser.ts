ce28ab92dde157dba3f5b2377a57cac1876ae5e3
Luis Vivia <luisvivia@MacBook-Pro-de-Luis-10.local>
2025-12-15 18:28:29 -0600

    a

--- app/composables/useBackendUser.ts
+++ app/composables/useBackendUser.ts
@@ -28,7 +28,7 @@ export function useBackendUser() {
         return
       }
 
-      await $fetch('https://tocho5-api.tochero5.mx/api/me', {
+      await $fetch('http://tocho5-api.tochero5.mx/api/me', {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${token}`,
